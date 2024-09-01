e2efbdc0ca2d8add3c63bc7d3567f0fffde2a9eb
HaiPaya <haipaya@dzdgame.cn>
2024-09-01 19:15:10 +0800

    初始提交

---
+++ src/main/java/cn/dzdstudio/mc/liteScriptEngine/LiteScriptEngine.java
@@ -0,0 +1,15 @@
+package cn.dzdstudio.mc.liteScriptEngine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LiteScriptEngine extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
